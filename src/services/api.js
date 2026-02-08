ddcdd0b5e0513a621aba35de5aec96b77678976e
jaures nguembu <jauresnguembu@gmail.com>
2026-02-08 22:24:41 +0100

    ajout du backend en lignes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios'
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/'
+const API_BASE_URL = 'https://terrabia-mobile.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
